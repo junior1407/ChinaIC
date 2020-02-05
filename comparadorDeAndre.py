28339732edf483255d97691791ee47722fcfe724
ValdirJr <vsj@ic.ufal.br>
2020-02-05 17:41:07 -0300

    ok

--- comparadorDeAndre.py
+++ comparadorDeAndre.py
@@ -1,5 +1,3 @@
- from imutils import face_utils
-import dlib
 import cv2
 import numpy as np
 import lib
@@ -12,88 +10,21 @@ from dbClass import Database
 import shutil
 bd = Database()
 lista = os.listdir('poses')
-print(lista)
 for l in lista:
     print(l)
     currImg = cv2.imread('poses/'+ l, cv2.IMREAD_COLOR)
+    bd.addImg(currImg)
    # currImg = cv2.resize(currImg, (0, 0), fx=0.5, fy=0.5)
     
-    facesDetected, scores, idx = lb.getFaceRects(currImg, 0)
-    faceDescriptors = lb.getFaceDescriptors(currImg, facesDetected)
-    if (len(faceDescriptors) != len(facesDetected)):
-        print("Putz")
-    i=0
-    for candidatoFd in faceDescriptors:
-        if (scores[i] < 0.22):
-            i+=1
-            continue
-        x, y, w, h = facesDetected[i].left(), facesDetected[i].top(), facesDetected[i].width(), facesDetected[i].height()
-        tl = (x,y)
-        br = (x + w, y + h)
-        height, width = currImg.shape[:2]
-        resolution =(width, height)
-      #  print(resolution)
-        if (lb.isFaceValid(tl,br,resolution) is False):
-            continue
-                
-            # plt.figure()
-           # plt.subplot(121)
-            
-           # plt.subplot(122)
-           # plt.show()
-            
-        if (len(bd['faces'].keys()) == 0):
-            print("Adicionou")
-            bd['faces'][idNumber] = []
-            bd['faces'][idNumber].append((candidatoFd, currImg[y:y+h, x:x+w], l))
-            idNumber+=1
-        else:
-            print("Start")
-            diferente = 1
-            stop = 0
-            for k,v in bd['faces'].items(): #Iterando pessoas
-                distances = []
-                if (stop == 1):
-                    break
-                for elem in v: # Iterando fotos de uma pessoa
-                    distance = lb.faceDistance(candidatoFd, elem[0])
-                    plt.imshow(elem[1])
-                    #print(distance)
-                    distances.append(distance)
-                minima = np.min(distances)
-                if (minima > 0.5):
-                    pass
-                    #print("Pessoa diferente")
-                    #bd[idNumber] = []
-                    #bd[idNumber].append((candidatoFd, currImg[y:y+h, x:x+w]))
-                    #idNumber+=1
-                    #break
-                elif (minima >= 0.1 and minima <= 0.49):
-                    diferente = 0
-                    stop = 1
-                    print("Igual, mas vale add")
-                    bd['faces'][k].append((candidatoFd, currImg[y:y+h, x:x+w], l))
-                    break
-                else:            
-                    diferente = 0
-                    stop = 1
-                  #  print("Igual, mas faça nada")
-            if (diferente ==1):
-                print("Adicionou")
-                bd['faces'][idNumber] = []
-                bd['faces'][idNumber].append((candidatoFd, currImg[y:y+h, x:x+w], l))
-                idNumber+=1           
-        i+=1
-        pass
     
 shutil.rmtree('bd', ignore_errors=True)
 os.mkdir('bd')
-for k,v in bd['faces'].items():
-    i=0
-    os.mkdir('bd/'+str(k))
-    for elem in v:
-        cv2.imwrite('bd/'+str(k)+'/'+str(i)+'.jpg', elem[1])
-        i+=1
+#for k,v in bd['faces'].items():
+ #   i=0
+  #  os.mkdir('bd/'+str(k))
+   # for elem in v:
+    #    cv2.imwrite('bd/'+str(k)+'/'+str(i)+'.jpg', elem[1])
+     #   i+=1
 
 
 #euclidean_distance = np.linalg.norm(np.array(descript1)-np.array(descript2))
--- dbClass.py
+++ dbClass.py
@@ -1,24 +1,38 @@
 import libTop as lb
+import numpy as np
 class Database:
+    
     def __init__(self):
          self.nextId = 0
          self.faces = {}
-    def addImg(img, upsample=0, minimumScore = 0.22):
+    def findBestClass(self, faceDescriptor):
+        #distances  = [(iD, lb.faceDistance(faceDescriptor, entry[0])) for iD, entries in self.faces for entry in entries]
+        
+        print()
+        pass
+            
+    def addFace(self, faceDescriptor, imgCropped):
+         if (len(self.faces.keys()) == 0):
+            self.faces[self.nextId]= np.array([[faceDescriptor, imgCropped]])
+         else:
+             pass
+
+    def addImg(self, img, upsample=0, minimumScore = 0.22):
         facesDetected, scores, idx = lb.getFaceRects(img, 0)
         faceDescriptors = lb.getFaceDescriptors(img, facesDetected)
         for i in range(len(facesDetected)):
             #If the detection doesn't have a score big enough, ignore it.
-            if (score[i]< minimumScore):
+            if (scores[i]< minimumScore):
                 continue
 
             #If the part of the face is not visible, ignore it.
             if (lb.isFaceValid(img, facesDetected[i]) is False):
                 continue
-            x, y, w, h = facesDetected.left(),facesDetected.top(),facesDetected.width(), facesDetected.height()
-            addFace(faceDescriptors[i], img[y:y+h, x:x+w])
+            x, y, w, h = facesDetected[i].left(),facesDetected[i].top(),facesDetected[i].width(), facesDetected[i].height()
+            self.addFace(faceDescriptors[i], img[y:y+h, x:x+w])
     
-    def addFace(faceDescriptor, imgCropped):
-        if (len(face))
+   
+            
         
         
 
